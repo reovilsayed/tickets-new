451ab9b126e5b1567fcba2089ebd3a4b4fd82471
Kazi-Rayhan <thisiskazi@gmail.com>
2024-08-02 18:16:21 +0600

    done

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/offer-to-car',
-        'payment-callback/{type}'
+        '/payment-callback/generic'
     ];
 }
