d80f50652244594ed85bc877e64dbfdc9e2e66d9
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2024-08-06 23:31:52 +0600

    issue

--- resources/views/components/app/user_sidebar.blade.php
+++ resources/views/components/app/user_sidebar.blade.php
@@ -8,23 +8,25 @@
                     <ul class="sidebar-dashboard">
                         <li class="{{ Route::is('user.dashboard') ? 'active-class' : '' }}"><a
                                 href="{{ route('user.dashboard') }}"><i class="fa-solid fa-house fa-1x"></i>
-                                Dashboard</a></li>
+                                {{ __('words.dashboard') }}</a></li>
                         <li class=" {{ Route::is('user.ordersIndex') ? 'active-class' : '' }}"><a
                                 href="{{ route('user.ordersIndex') }}"><i class="fa-solid fa-chart-simple fa-1x"></i>
-                                Orders</a></li>
+                                {{ __('words.orders') }}</a></li>
 
 
                         <li class="{{ Route::is('user.change_password') ? 'active-class' : '' }}"><a
                                 href="{{ route('user.change_password') }}"><i class="fa-solid fa-key"></i>
-                                Security</a>
+                                {{ __('words.security') }}</a>
                         </li>
                         <li class="">
-                            <form action="{{ route('logout') }}" id="logoutform" method="post" style="display: inline-block">
+                            <form action="{{ route('logout') }}" id="logoutform" method="post"
+                                style="display: inline-block">
                                 @csrf
-                               
+
                             </form>
-                            <button onclick="document.getElementById('logoutform').submit()" type="submit" class="user-log"><i class="fa-solid fa-user"></i>
-                                logout</button>
+                            <button onclick="document.getElementById('logoutform').submit()" type="submit"
+                                class="user-log"><i class="fa-solid fa-user"></i>
+                                {{ __('words.logout') }}</button>
                         </li>
                     </ul>
                 </div>
