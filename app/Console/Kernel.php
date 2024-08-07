cc700edc8c1396034c80355aa2a9f3f4de833287
sayed khan <reovilsayed@gmail.com>
2024-08-07 09:00:53 +0600

    command delete

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('charge:products')->daily();
-        $schedule->command('queue:work --stop-when-empty')->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
