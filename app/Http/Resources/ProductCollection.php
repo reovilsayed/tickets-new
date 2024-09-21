5d767dcaa56856c71a9de187153966c7242e6b37
KawshikBss <kawshikbss@gmail.com>
2024-09-21 19:48:17 +0600

    created resource for product collection

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'data' => $this->collection->map(fn($product) => new ProductResource($product)),
+            'total' => $this->total(),
+            'count' => $this->count(),
+            'per_page' => $this->perPage(),
+            'current_page' => $this->currentPage(),
+            'last_page' => $this->lastPage(),
+        ];
+    }
+}
