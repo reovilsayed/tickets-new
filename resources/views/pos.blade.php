1acd1674c28bd18cd71b2d184e2505437590f6ff
KawshikBss <kawshikbss@gmail.com>
2024-09-22 00:26:47 +0600

    created pos template

---
+++ resources/views/pos.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Essência Company</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        crossorigin="anonymous" />
+    @viteReactRefresh
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
