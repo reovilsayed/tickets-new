cc68276aaf98b7faf6c844b517e780425ccf038a
KawshikBss <kawshikbss@gmail.com>
2024-09-22 11:29:49 +0600

    new build

---
+++ public/build/assets/utils-ed3e4056.js
@@ -0,0 +1,1 @@
+const s="/build/assets/no-image-b9e79929.jpg",r=(t,a)=>{const o=new Date(t),n=new Date(a),e={day:"numeric",month:"short",year:"numeric"};return`${o.toLocaleDateString("en-US",e)} - ${n.toLocaleDateString("en-US",e)}`},c=t=>parseFloat(t).toFixed(2),i=t=>t??s;export{r as a,c as f,i as v};
