63d682dc2e5f9ac77252e2e86de3cdd477a3c4ff
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2024-09-23 23:36:32 +0600

    done

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Models\Role as ModelsRole;
 
-class Role extends Model
+class Role extends ModelsRole
 {
     use HasFactory;
 }
