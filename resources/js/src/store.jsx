f60982ae240beea5a6ed078d8820a483b999fdf0
KawshikBss <kawshikbss@gmail.com>
2024-09-25 20:40:16 +0600

    added filter slice

--- resources/js/src/store.jsx
+++ resources/js/src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemInfoModalReducer from "./lib/features/itemInfoModalSlice";
 import searchQueryReducer from "./lib/features/searchQuerySlice";
+import filterReducer from "./lib/features/filterSlice";
 
 export const store = configureStore({
     reducer: {
         itemInfoModal: itemInfoModalReducer,
         searchQuery: searchQueryReducer,
+        filter: filterReducer,
     },
 });
