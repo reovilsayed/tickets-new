53741ffb1c8d0c1a8f2aeeb0ba5357b8c106dd39
KawshikBss <kawshikbss@gmail.com>
2024-09-27 14:31:28 +0600

    created cart info for payment modal

---
+++ resources/js/src/components/dashboard/PaymentModal/CartInfo/CartInfoItem.jsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useMemo } from "react";
+import { calculateExtrasFees } from "../../../../lib/utils";
+
+function CartInfoItem({ item }) {
+    const extrasFees = useMemo(
+        () => calculateExtrasFees(item?.extras) ?? 0.0,
+        item
+    );
+    const calculateSingleExtra = useCallback(
+        (extra) => {
+            return extra?.newQuantity && extra?.quantity
+                ? (extra?.newQuantity - extra?.quantity) * extra?.price
+                : extra?.newQuantity
+                ? extra?.newQuantity * extra?.price
+                : 0.0;
+        },
+        [item]
+    );
+    return (
+        <>
+            <tr className="table-info">
+                <td>{item?.name}</td>
+                <td>{item?.price}$</td>
+                <td>{item?.quantity}</td>
+                <td>{item?.itemTotal}$</td>
+                <td>{extrasFees}$</td>
+                <td className="text-end">{item?.itemTotal}$</td>
+            </tr>
+            {item?.extras?.map((extra, index) => (
+                <tr key={index}>
+                    <td>{extra?.name}</td>
+                    <td>{extra?.price}$</td>
+                    <td>
+                        {extra?.quantity ?? 0}{" "}
+                        {extra?.newQuantity
+                            ? `+ ${extra?.newQuantity - (extra?.quantity ?? 0)}`
+                            : ""}
+                    </td>
+                    <td colSpan={3} className="text-end">{calculateSingleExtra(extra)}$</td>
+                </tr>
+            ))}
+        </>
+    );
+}
+
+export default CartInfoItem;
