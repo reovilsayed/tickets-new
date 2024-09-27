9810d9db4430799525249825e0cb81f1ebc8ceaf
KawshikBss <kawshikbss@gmail.com>
2024-09-27 14:30:16 +0600

    added new slices

--- resources/js/src/store.jsx
+++ resources/js/src/store.jsx
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemInfoModalReducer from "./lib/features/itemInfoModalSlice";
 import searchQueryReducer from "./lib/features/searchQuerySlice";
+import paymentModalReducer from "./lib/features/paymentModalSlice";
+import addExtraModalReducer from "./lib/features/addExtraModalSlice";
 import filterReducer from "./lib/features/filterSlice";
 
 export const store = configureStore({
     reducer: {
         itemInfoModal: itemInfoModalReducer,
+        addExtraModal: addExtraModalReducer,
         searchQuery: searchQueryReducer,
+        paymentModal: paymentModalReducer,
         filter: filterReducer,
     },
 });
