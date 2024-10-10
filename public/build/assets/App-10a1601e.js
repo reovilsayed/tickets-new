482ca1e0b0ec96591e30927319f35c748a8de1af
KawshikBss <kawshikbss@gmail.com>
2024-10-10 18:00:32 +0600

    new build

---
+++ public/build/assets/App-10a1601e.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-6cc9246f.js";function r(){return console.log("hello"),o.jsx("h1",{children:"Home page"})}export{r as default};
--- public/build/assets/App-17092535.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-d97bf9ce.js";function r(){return console.log("hello"),o.jsx("h1",{children:"Home page"})}export{r as default};
