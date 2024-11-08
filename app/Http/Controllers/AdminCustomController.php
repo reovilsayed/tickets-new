911296075274380a099bad198b1e6c5d4a941248
sayed khan <reovilsayed@gmail.com>
2024-11-08 23:16:05 +0600

    df

--- app/Http/Controllers/AdminCustomController.php
+++ app/Http/Controllers/AdminCustomController.php
@@ -446,22 +446,7 @@ class AdminCustomController extends Controller
        $order->payment_status = 1;
        $order->status = 1;
        $order->save();
-        $products = $order->tickets->groupBy('product_id');
-        foreach ($products as $key => $tickets) {
-            $product = Product::find($key);
 
-            if ($order->user) {
-                Mail::to($order->user->email)->send(new TicketDownload($order, $product, null));
-            } else {
-
-                if ($order->payment_method == 'invite') {
-
-                    Mail::to($order->user->email)->send(new InviteDownload($order, $product, null));
-                } else {
-                    Mail::to($order->user->email)->send(new TicketDownload($order, $product, null));
-                }
-            }
-        }
    
        $toco = new TOCOnlineService;
        $response = $toco->createCommercialSalesDocument($order);
