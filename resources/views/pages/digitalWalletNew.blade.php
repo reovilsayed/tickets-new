0446518d445dd10734bacdcddee0994401957b2c
sayed khan <reovilsayed@gmail.com>
2024-11-21 16:45:25 +0600

    event id adjustment

--- resources/views/pages/digitalWalletNew.blade.php
+++ resources/views/pages/digitalWalletNew.blade.php
@@ -119,18 +119,18 @@
                     {{ $event->name }}
                 </h3>
                 <div class="cus-btn-bar">
-                    <a class="cus-btn" href="{{ route('digital-wallet', $user) }}"><i class="fa-solid fa-qrcode"></i>
+                    <a class="cus-btn" href="{{ route('digital-wallet', ['user' => $user ,'event_id'=>request('event_id')]) }}"><i class="fa-solid fa-qrcode"></i>
                         {{ __('words.tickets') }}</a>
                     @if ($orders->count())
                         <a class="cus-btn"
-                            href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'invoice']) }}"><i
+                            href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'invoice' ,'event_id'=>request('event_id')]) }}"><i
                                 class="fa-solid fa-file-invoice"></i>
                             {{ __('words.invoice') }}</a>
                     @endif
-                    <a class="cus-btn" href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'info']) }}"><i
+                    <a class="cus-btn" href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'info' ,'event_id'=>request('event_id')]) }}"><i
                             class="fa-solid fa-circle-info"></i>
                         {{ __('words.information') }}</a>
-                    <a class="cus-btn" href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'products']) }}"><i
+                    <a class="cus-btn" href="{{ route('digital-wallet', ['user' => $user, 'tab' => 'products','event_id'=>request('event_id')]) }}"><i
                             class="fa-solid fa-box"></i>
                         {{ __('words.products') }}</a>
                 </div>
