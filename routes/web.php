7a262f0b224f465515ed692cda47fe4dc834e663
sayed khan <reovilsayed@gmail.com>
2025-01-16 22:25:17 +0600

    d

--- routes/web.php
+++ routes/web.php
@@ -342,6 +342,7 @@ Route::get('/my-wallet/{user:uniqid}', function (User $user, Request $request) {
         // Fetch the user's orders excluding those with 'invite' as the payment method
         $orders = Order::where('user_id',$user->id)->where('event_id',$event->id)
             ->where('payment_method', '!=', 'invite')
+            ->latest()
             ->get();
 
         // Fetch the user's tickets for the selected event and with a non-null order ID
