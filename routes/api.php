4d39b61dcbf323cd5e2f468bcb0f3ca6882956ce
sayed khan <reovilsayed@gmail.com>
2025-01-17 21:39:25 +0600

    c

--- routes/api.php
+++ routes/api.php
@@ -138,6 +138,7 @@ Route::post('/extras-scan-ticker', function (Request $request) {
     try {
         if (Hash::check(env('SECURITY_KEY'), $request->checksum)) {
             $ticket = Ticket::where('ticket', $request->ticket)->where('active',1)->first();
+            if (!$ticket) throw new Exception(__('words.ticket_not_active'));
             $extras = [];
 
             foreach ($ticket->extras as $extra) {
