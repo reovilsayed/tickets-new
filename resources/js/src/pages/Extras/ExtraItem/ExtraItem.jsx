dfcb464bfeb3573c17fa8430abdff130db53bbf5
sayed khan <kawshikbss@gmail.com>
2025-02-21 19:02:57 +0600

    reverted changes

--- resources/js/src/pages/Extras/ExtraItem/ExtraItem.jsx
+++ resources/js/src/pages/Extras/ExtraItem/ExtraItem.jsx
@@ -6,82 +6,83 @@ import { open } from "../../../lib/features/itemInfoModalSlice";
 import { useCart } from "react-use-cart";
 
 const ExtraItem = ({ extra }) => {
-  const dispatch = useDispatch();
-  const openInfoModal = () => {
-    dispatch(open(extra));
-  };
+    const dispatch = useDispatch();
+    const openInfoModal = () => {
+        dispatch(open(extra));
+    };
 
-  const { addItem, removeItem, inCart } = useCart();
-  const active = inCart(extra?.id);
+    const { addItem, removeItem, inCart } = useCart();
+    const active = inCart(extra?.id);
 
-  return (
-    <div
-      className={`ticket-item-container col-lg-2 col-md-3 col-sm-3 col-6 my-hover-effect g-3 ${
-        active ? "active" : ""
-      }`}
-      onClick={() => addItem(extra)}
-    >
-      <div className="extra-item-card">
-        {active ? (
-          <button
-            className="delete-btn"
-            onClick={() => {
-              removeItem(extra.id);
-            }}
-          >
-            <i className="fa fa-times"></i>
-          </button>
-        ) : (
-          ""
-        )}
-        <div className="custom-card">
-          <span className="extra-item-badge">
-            {formatPrice(extra?.sale_price ? extra?.sale_price : extra?.price)}
-            {" €"}
-          </span>
+    return (
+        <div
+            className={`ticket-item-container col-lg-2 col-md-3 col-sm-3 col-6 my-hover-effect g-3 ${
+                active ? "active" : ""
+            }`}
+            onClick={() => addItem(extra)}
+        >
+            <div className="extra-item-card">
+                {active ? (
+                    <button
+                        className="delete-btn"
+                        onClick={() => {
+                            removeItem(extra.id);
+                        }}
+                    >
+                        <i className="fa fa-times"></i>
+                    </button>
+                ) : (
+                    ""
+                )}
+                <div className="custom-card">
+                    <span className="extra-item-badge">
+                        {formatPrice(
+                            extra?.sale_price ? extra?.sale_price : extra?.price
+                        )}
+                        {" €"}
+                    </span>
 
-          <div className="card-header position-relative">
-            {extra?.thumbnail ? (
-              <img
-                className="extra-item-img"
-                src={
-                  extra?.thumbnail
-                    ? `https://events.essenciacompany.com/storage/${extra?.thumbnail}`
-                    : "/images/no-image.jpg"
-                }
-                alt={extra.display_name}
-              />
-            ) : (
-              <div className="extra-item-img">
-                <i class="fas fa-cocktail fa-2x"></i>
-              </div>
-            )}
-            {extra?.event?.name && (
-              <span className="extra-item-unit">
-                {extra?.event?.name.length > 15
-                  ? `${extra.event.name.substring(0, 15)}...`
-                  : extra?.event?.name}
-              </span>
-            )}
-          </div>
+                    <div className="card-header position-relative">
+                        {extra?.thumbnail ? (
+                            <img
+                                className="extra-item-img"
+                                src={extra?.thumbnail ?? "/images/no-image.jpg"}
+                                alt={extra.display_name}
+                            />
+                        ) : (
+                            <div className="extra-item-img">
+                                <i class="fas fa-cocktail fa-2x"></i>
+                            </div>
+                        )}
+                        {extra?.event?.name && (
+                            <span className="extra-item-unit">
+                                {extra?.event?.name.length > 15
+                                    ? `${extra.event.name.substring(0, 15)}...`
+                                    : extra?.event?.name}
+                            </span>
+                        )}
+                    </div>
 
-          <div className="card-body extra-item-body">
-            <p title={extra.display_name} className="extra-item-name">
-              {extra?.display_name.length > 15
-                ? `${extra.display_name.substring(0, 15)}...`
-                : extra.display_name}
-            </p>
-            {/* <button
+                    <div className="card-body extra-item-body">
+                        <p
+                            title={extra.display_name}
+                            className="extra-item-name"
+                        >
+                            {extra?.display_name.length > 15
+                                ? `${extra.display_name.substring(0, 15)}...`
+                                : extra.display_name}
+                        </p>
+                        {/* <button
                             onClick={openInfoModal}
                             className="btn btn-primary btn-sm position-absolute rounded-circle extra-item-info-btn"
                         >
                             <i className="fa fa-info"></i>
                         </button> */}
-          </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ExtraItem;
