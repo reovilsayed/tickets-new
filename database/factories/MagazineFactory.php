7239b90d56ad816760eb9400c32022148cc3441a
Jayed Khan <jayed@outlook.com>
2025-03-18 18:13:41 +0600

    magazine model,migration, factory and seeder created

---
+++ database/factories/MagazineFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Magazine>
+ */
+class MagazineFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'image' => $this->faker->randomElement(Storage::files('magazines/test')) ,
+            'description' => $this->faker->paragraph(),
+        ];
+    }
+}
