e5312c642bc764401805a97f69647b5aed59f3a5
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2025-04-21 18:22:52 +0600

    adas

--- resources/views/auth/user/pdf_view.blade.php
+++ resources/views/auth/user/pdf_view.blade.php
@@ -72,7 +72,7 @@
 <script>
     // URL of your PDF (make sure nid.pdf is in same folder)
     const url = '{{Storage::url($archive->pdf_view)}}';
-    console.log('url')
+    console.log(url)
 
     // Setting worker
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
