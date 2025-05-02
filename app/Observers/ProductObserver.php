35f457206f77b62d4ae9f40124d411c60d5763cd
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2025-05-02 23:42:43 +0600

    done

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -24,7 +24,6 @@ class ProductObserver
         );
 
         if (isset($data['error'])) {
-            dd($data['message']);
             Log::error('TOCOnlineService: ' . $data['message']);
             return;
         }
