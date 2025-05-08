659a652c35e0f05394a1a50d0a3bc1eb06084391
KawshikBss <kawshikbss@gmail.com>
2025-05-08 15:43:19 +0600

    asd

--- app/Http/Controllers/AppApiController.php
+++ app/Http/Controllers/AppApiController.php
@@ -420,7 +420,7 @@ class AppApiController extends Controller
     function getWalletCustomer()
     {
         $customer = null;
-        return dd(request()->filled('qr'));
+        return dd(request()->filled('user'));
         if (request()->filled('user')) {
             $customer = User::where(function ($query) {
                 $query->where('email', request()->user)->orWhere('contact_number', request()->user);
