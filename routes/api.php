8ce62ff76ea28fc3f155316e361fdf6df711bffa
KawshikBss <kawshikbss@gmail.com>
2025-05-10 15:29:00 +0600

    SD

--- routes/api.php
+++ routes/api.php
@@ -184,6 +184,6 @@ Route::group(['prefix' => 'app'], function () {
         Route::post('order/create', [AppApiController::class, 'createOrder']);
         Route::get('wallet', [AppApiController::class, 'getMyWallet']);
         Route::post('wallet/customer', [AppApiController::class, 'getWalletCustomer']);
-        Route::post('user-from-qr', [AppApiController::class, 'getUserFromQr']);
     });
+    Route::post('user-from-qr', [AppApiController::class, 'getUserFromQr']);
 });
