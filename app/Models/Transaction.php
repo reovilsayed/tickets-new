a3f40f37a4e1d87a94989a1a28ef3e7f145960a5
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2025-05-17 19:27:13 +0600

    done

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Transaction extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('withTransactionable', function (Builder $builder) {
+            $builder->whereHas('transactionable');
+        });
+    }
 
     public function transactionalbe()
     {
